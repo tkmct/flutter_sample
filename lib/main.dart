4969ada099af95f7307b844ba3d51fcdacedb996
tkmct <1220t.takamichi@gmail.com>
2018-12-08 02:09:16 +0900

    Add button section

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,35 @@ class MyApp extends StatelessWidget {
           ],
         ));
 
+    Column buildButtonColumn(IconData icon, String label) {
+      Color color = Theme.of(context).primaryColor;
+
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(icon, color: color),
+          Container(
+              margin: const EdgeInsets.only(top: 8.0),
+              child: Text(label,
+                  style: TextStyle(
+                      fontSize: 12.0,
+                      fontWeight: FontWeight.w400,
+                      color: color)))
+        ],
+      );
+    }
+
+    Widget buttonSection = Container(
+        child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        buildButtonColumn(Icons.call, 'CALL'),
+        buildButtonColumn(Icons.near_me, 'ROUTE'),
+        buildButtonColumn(Icons.share, 'SHARE'),
+      ],
+    ));
+
     return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(primarySwatch: Colors.cyan),
@@ -45,7 +74,8 @@ class MyApp extends StatelessWidget {
               height: 240.0,
               fit: BoxFit.cover,
             ),
-            titleSection
+            titleSection,
+            buttonSection
           ]),
         ));
   }
